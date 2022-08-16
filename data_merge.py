6d1988b2763f3d5c5dde335d6ea38e849113b83a
bjasperson <ben.jasperson@gmail.com>
2022-08-16 07:49:35 -0500

    data_merge.py: -final_comsol_results.csv now saves with ext_ratio, insert_loss and Temp -minor updates to docstrings, comments; verbiage updates to input messages

--- data_merge.py
+++ data_merge.py
@@ -17,31 +17,33 @@ def main():
     if input("process input data (y/n)? ")=="y":
         readme = ""
         base_folder = input("prod[x]_data folder:  ")
-        #base_folder = "/home/jaspers2/Documents/pixel_optimization/prod2_data/"
         image_folder = base_folder + "/images" 
         results_folder = base_folder + "/results"
         
+        #timestamp/make directory
         timestamp = image_creation.create_timestamp()
         combined_results_folder = base_folder + "/combined_results_" + timestamp + "/"
         os.mkdir(combined_results_folder)
+        
         #import all files in folder
         df_images = import_images(image_folder)
         df_results = import_results(results_folder)
         
+        #
         df_combined = combined_results(df_images,df_results)
-        df_combined,np_combined_images,np_reduced_images,df_combined_results,readme = nn_config(df_combined,readme)
+        df_combined, np_combined_images, np_reduced_images, df_combined_results, readme = nn_config(df_combined,readme)
         save_results(combined_results_folder, df_combined, np_combined_images, np_reduced_images, df_combined_results, readme)
     
     if input("analyze results (y/n)?  ")=="y":
         check = "combined_results_folder" in locals()
         if check == False:
             combined_results_folder = input("combined results folder: ")
-        analyze_results(combined_results_folder)
-        
+        analyze_results(combined_results_folder)  
         
 
 def import_images(folder):
-    
+    """import image files
+    """
     df = pd.DataFrame()
     for file in os.listdir(folder):
         images_in = np.load(os.path.join(folder,file))
@@ -57,8 +59,10 @@ def import_images(folder):
     df = df.set_index('id')
     return df
 
+
 def import_results(folder):
-    #returns df of results from text files in folder
+    """combines text file results from results folder, returns df
+    """
     
     #try this next: https://python-bloggers.com/2021/09/3-ways-to-read-multiple-csv-files-for-loop-map-list-comprehension/
     #read tarfiles directly: https://stackoverflow.com/questions/2018512/reading-tar-file-contents-without-untarring-it-in-python-script
@@ -73,7 +77,8 @@ def import_results(folder):
     return df
 
 def analyze_results(file_path):
-    #perform any analysis on results as needed
+    """perform any analysis on results as needed
+    """
     df = pd.read_pickle(file_path+"/df_all.pkl")
     df.plot('T_VO2_avg','q_applied',kind="scatter")
     df.plot("ext_ratio","T_VO2_avg",kind="scatter")
@@ -95,18 +100,20 @@ def analyze_results(file_path):
     df.plot("coverage","Tr_met",kind="scatter")
     df.plot("coverage","T_VO2_avg",kind="scatter")
     
-        
-
 
 def combined_results(images,results):
+    """creates one df with images and results synced up
+    """
     combined = pd.concat([images,results],axis=1).dropna(axis=0)    
     combined["ext_ratio"] = 10*np.log10(combined["Tr_ins"]/combined["Tr_met"])
     combined["insert_loss"] = 10*np.log10(1/combined["Tr_ins"])
     
     return combined
 
+
 def nn_config(combined_df,readme):
-    #saves the combined feature_images.npy
+    """saves the combined feature_images.npy
+    """
     
     #temporarily remove any Tr_ins < 0.5 and ext_ratio < 0
     if input("remove Tr_ins<0.5 and ext_ratio<0? y/n:  ")=="y":
@@ -114,9 +121,9 @@ def nn_config(combined_df,readme):
         combined_df = combined_df[combined_df["Tr_ins"]>0.5]
         readme += 'removed Tr_ins < 0.5 and ext_ratio < 0\n'
         
-    if input("keep Tr_met<0.2? y/n:  ")=="y":
+    if input("remove Tr_met>0.2? y/n:  ")=="y":
         combined_df = combined_df[combined_df["Tr_met"]<0.2]
-        readme += 'only kept Tr_met < 0.2\n'
+        readme += 'removed Tr_met > 0.2\n'
     
     
     #clean up for numpy array
@@ -130,16 +137,19 @@ def nn_config(combined_df,readme):
     np_reduced_images = np_images[:,:,:H//2,:W//2]
     
     #saves the combined csv: R_ins,R_met,Tr_ins,Tr_met,A_ins,A_met,Temp
-    np_results = combined_df[['R_ins','R_met','Tr_ins','Tr_met','A_ins','A_met','T_VO2_avg']]
-    np_results = np_results.rename(columns={'T_VO2_avg':'Temp'})
+    df_results = combined_df[['ext_ratio','insert_loss','T_VO2_avg']]
+    df_results = df_results.rename(columns={'T_VO2_avg':'Temp'})
     
-    return combined_df,np_images,np_reduced_images,np_results,readme
+    return combined_df,np_images,np_reduced_images,df_results,readme
+
 
 def save_results(path, df_all, np_images, np_reduced_images, df_results, readme):
+    """saves all results
+    """
     df_all.to_pickle(path + 'df_all.pkl')
     np.save(path + 'full_feature_images',np_images)
     np.save(path + 'feature_images',np_reduced_images)
-    df_results.to_csv(path + 'final_comsol_results.csv', columns=['Tr_ins','Tr_met','Temp'], index=False)
+    df_results.to_csv(path + 'final_comsol_results.csv', columns=['ext_ratio','insert_loss','Temp'], index=False)
     
     with open(os.path.join(path,'readme.txt'),'w') as output:
         output.write(readme)
